a6e8b5a0173710d9cc58312740c739275709eba3
Adelle Low <adellelow02@gmail.com>
2023-03-15 16:27:30 +0800

    Search Bar v1 test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="home">
     <h1>This is a Home page</h1>
-
+    <SearchBar />
   </div>
 </template>
 
-<script setup>
+<script>
+import SearchBar from '@/components/SearchBar/SearchBar.vue'
 
-
+export default {
+  components: {
+    SearchBar
+  }
+}
 </script>
+
 
 <style>
 
